8c4e82b803ab97c0accf8303311060c9fa37f5c1
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-25 18:10:50 +0530

    issue in the layout and grid method

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css'
 
+
+function Cell({ filled, onClick }) {
+  return <button type='button' onClick={onClick} className={filled ? "cell cell-activated" : "cell"}></button>
+}
+
 function App() {
+
+  const config = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+  ]
+
+  const activeCells = (index) =>
+  {
+
+  }
 
   return (
     <div>
       <h1>Grid Lights</h1>
+      <div className='grid' style={{
+        gridTemplateColumns : `repeat(${config[0].length}, 1fr)`
+        }}>
+        {
+          config.flat(1).map((value, index) => value==1 && <Cell key={index} filled={false} onClick={() => activeCells(index)}/>)
+        }
+      </div>
     </div>
   )
 }
