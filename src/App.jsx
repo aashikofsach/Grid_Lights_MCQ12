1359c2ad9c19c6bbdc746f45f69bf6a9a026f500
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-26 19:06:37 +0530

    issue in deactivateCells function code

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 
-function Cell({ filled, onClick }) {
-  return <button type='button' onClick={onClick} className={filled ? "cell cell-activated" : "cell "}></button>
+function Cell({ filled, onClick , isDisabled }) {
+  return <button type='button' onClick={onClick} className={filled ? "cell cell-activated" : "cell"  } disabled={isDisabled}></button>
 }
 
 function App() {
 
   const [order , setOrder] = useState([]);
+  // const [isDeactivating , setIsDeactivating] = useState(false);
 
   const config = [
     [1, 1, 1],
@@ -29,6 +30,23 @@ function App() {
     
     function deactivateCells()
     {
+      // setIsDeactivating(true);
+      const timer = setInterval(()=>{
+        setOrder((prev)=> {
+          const newOrder = prev.slice()
+          newOrder.pop()
+          return newOrder;
+        })
+
+        if(newOrder.length ===0)
+        {
+          clearInterval(timer);
+          // setIsDeactivating(false)
+
+        }
+        // return newOrder;
+
+      },300)
 
     }
   }
@@ -42,7 +60,7 @@ function App() {
 
       }}>
         {
-          config.flat(1).map((value, index) => value === 1 ? <Cell key={index} filled={order.includes(index)} onClick={() => activeCells(index)} /> : <span></span>)
+          config.flat(1).map((value, index) => value === 1 ? <Cell key={index} filled={order.includes(index)} onClick={() => activeCells(index)} isDisabled={order.includes(index)}/> : <span></span>)
         }
       </div>
     </div>
