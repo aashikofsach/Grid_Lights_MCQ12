ecf63c58a0920440b937c32f041b5bc7eb897864
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-07-26 17:27:47 +0530

    clciking on boxes to gets filled with green color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 
 
 function Cell({ filled, onClick }) {
-  return <button type='button' onClick={onClick} className={filled ? "cell cell-activated" : "cell"}></button>
+  return <button type='button' onClick={onClick} className={filled ? "cell cell-activated" : "cell "}></button>
 }
 
 function App() {
+
+  const [order , setOrder] = useState([]);
 
   const config = [
     [1, 1, 1],
@@ -13,23 +16,38 @@ function App() {
     [1, 1, 1]
   ]
 
-  const activeCells = (index) =>
-  {
+  const activeCells = (index) => {
+   const  newOrder = [...order , index];
+   console.log(newOrder)
+    setOrder(newOrder)
 
+    if(newOrder.length  === config.flat(1).filter((x)=> Boolean(x)).length)
+    {
+      deactivateCells() ;
+    }
+
+    
+    function deactivateCells()
+    {
+
+    }
   }
 
   return (
     <div>
       <h1>Grid Lights</h1>
       <div className='grid' style={{
-        gridTemplateColumns : `repeat(${config[0].length}, 1fr)`
-        }}>
+        gridTemplateColumns: `repeat(${config[0].length}, 1fr)`,
+        // gridTemplateRows: `repeat(${config[0].length}, 1fr)`
+
+      }}>
         {
-          config.flat(1).map((value, index) => value==1 && <Cell key={index} filled={false} onClick={() => activeCells(index)}/>)
+          config.flat(1).map((value, index) => value === 1 ? <Cell key={index} filled={order.includes(index)} onClick={() => activeCells(index)} /> : <span></span>)
         }
       </div>
     </div>
   )
 }
+
 
 export default App
